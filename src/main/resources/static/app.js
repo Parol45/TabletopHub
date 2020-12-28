7839419d0b7014c47c0e717ec10f1ffc0567567f
Parol45 <forlulz1488@gmail.com>
2020-12-28 04:27:13 +0300

    Fully implemented game ui Next goal is game logic.
    P.S. it was really funny how 'React.createElement()"" and "this" inside its constructor is not the same thing

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,26 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let selectedDot = null;
-const DaysEnum = Object.freeze({"nothing":0, "redDot":1, "blueDot":2, "redLine":3, "blueLine":4})
+console.clear();
 
-function connectDots() {
+var selectedDot = null;
+var board = new Array(11);
 
-}
+const figuresEnum = Object.freeze({
+    'nothing': "",
+    'dot':
+        <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+            <circle cx='8' cy='8' r='8' />
+        </svg>,
+    'horizontal line':
+        <svg viewBox="0 0 50 10" xmlns="http://www.w3.org/2000/svg">
+            <line x1="0" y1="7" x2="50" y2="7" />
+        </svg>,
+    'vertical line':
+        <svg viewBox="0 0 10 50" xmlns="http://www.w3.org/2000/svg">
+            <line x1="5" y1="0" x2="5" y2="50" />
+        </svg>
+});
 
-class BridgItDot extends React.Component {
-    render() {
-        if ((this.props.color === 'blue' && this.props.i % 2 === 1) || (this.props.color === 'red' && this.props.i % 2 === 0)) {
-            return React.createElement('div', {className: this.props.color + '-dot'},
-                <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx='5' cy='5' r='5' fill={this.props.color}/>
-                </svg>);
-        } else {
-            return React.createElement('div', {className: 'no-dot'},
-                <svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx='5' cy='5' r='5' fill='rgba(0, 0, 0, 0)'/>
-                </svg>);
+class BridgItCell extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: this.props.color,
+            elementName: this.props.elementName
         }
+        board[this.props.i][this.props.j] = this;
+        this.connectDots = this.connectDots.bind(this);
+    }
+
+    connectDots() {
+        board[1][1].setState({color: 'blue',elementName: 'vertical line'});
+        board[2][2].setState({color: 'red' ,elementName: 'vertical line'});
+        board[0][2].setState({color: 'blue',elementName: 'horizontal line'});
+        board[1][3].setState({color: 'red' ,elementName: 'horizontal line'});
+        if (selectedDot == null) {
+            selectedDot = this;
+        } else if (selectedDot !== this) {
+            console.log(selectedDot.props.i + " " + selectedDot.props.j);
+            console.log(this.props.i + " " + this.props.j);
+            this.setState({elementName: 'nothing'});
+            selectedDot = null;
+        }
+    }
+
+    render() {
+        return React.createElement('div', {
+                className: this.state.color + ' ' + this.state.elementName,
+                onClick: this.connectDots
+            },
+            figuresEnum[this.state.elementName]);
+
     }
 }
 
@@ -28,29 +62,37 @@ class BridgItTableRow extends React.Component {
 
     render() {
         let row = React.createElement('div', {className: 'bridg-it-row'}, []);
-        for (let i = 0; i < 11; i++) {
-            let dot = React.createElement(BridgItDot, {color: this.props.color, key: i, i: i});
-            row.props.children.push(dot);
+        for (let j = 0; j < 11; j++) {
+            let elementName, color = '';
+            if ((this.props.i + j) % 2 !== 0) {
+                color = this.props.i % 2 === 1 ? 'red' : 'blue';
+                elementName = 'dot';
+            } else {
+                elementName = 'nothing'
+            }
+            row.props.children.push(
+                React.createElement(BridgItCell, {
+                    key: this.props.i + '' + j,
+                    color: color,
+                    elementName: elementName,
+                    i: this.props.i,
+                    j: j
+                }));
         }
         return row;
     }
 }
 
 class BridgItTable extends React.Component {
-    constructor(props) {
-        super(props);
-        let board = new Array(11);
-        for (let i = 0; i < board.length; i++) {
-
-        }
-        this.state = { board: board };
-    }
 
     render() {
         let table = React.createElement('div', {className: 'bridg-it-table'}, []);
-        for (let i = 0; i < 11; i++) {
-            let line = React.createElement(BridgItTableRow, {color: i % 2 === 1 ? 'red' : 'blue', key: i});
-            table.props.children.push(line);
+        for (let i = 0; i < board.length; i++) {
+            board[i] = new Array(11);
+            table.props.children.push(React.createElement(BridgItTableRow, {
+                key: i,
+                i: i,
+            }));
         }
         return table;
     }
